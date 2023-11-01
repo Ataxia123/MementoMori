a383e82e8e0e1f0f05faaaa059d96c8caefd64a2
Ataxia123 <vp.ca94@gmail.com>
2023-11-01 09:32:46 -0500

    ui

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -8,23 +8,6 @@ module.exports = {
     themes: [
       {
         scaffoldEth: {
-          primary: "#93BBFB",
-          "primary-content": "#212638",
-          secondary: "#DAE8FF",
-          "secondary-content": "#212638",
-          accent: "#93BBFB",
-          "accent-content": "#212638",
-          neutral: "#212638",
-          "neutral-content": "#ffffff",
-          "base-100": "#ffffff",
-          "base-200": "#f4f8ff",
-          "base-300": "#DAE8FF",
-          "base-content": "#212638",
-          info: "#93BBFB",
-          success: "#34EEB6",
-          warning: "#FFCF72",
-          error: "#FF8863",
-
           "--rounded-btn": "9999rem",
 
           ".tooltip": {
